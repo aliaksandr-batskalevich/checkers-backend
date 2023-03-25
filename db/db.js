4868aee539b1e01e5039808726f0ec10daa34224
”Aliaksandr-Batskalevich” <aliaksandr.batskalevich@gmail.com>
2023-03-25 20:25:12 +0300

    added postgres password

--- db/db.js
+++ db/db.js
@@ -2,9 +2,12 @@ const pg = require('pg');
 const {Pool} = pg;
 
 const pool = new Pool({
-    user: 'postgres',
+    user: 'alex',           // for server in GCP
+    password: 'alex',       // for server in GCP
+
+    // user: 'postgres',        // for server in localhost
     // password: 'Marry1990',   // for server in localhost
-    password: 'postgres',       // for server in GCP
+
     host: 'localhost',
     port: 5432,
     database: 'checkers_database'
