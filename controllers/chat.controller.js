eb0959bfc4875d6e8de7e7d7cd7f4a15c020dc21
”Aliaksandr-Batskalevich” <aliaksandr.batskalevich@gmail.com>
2023-04-02 22:47:04 +0300

    added functions for create any connected to chat from single account.

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -1,96 +1,97 @@
+const DAL = require("../db/dal.js");
 const chatService = require('../services/chat.service.js');
+const tokenService = require("../token.service.js");
+const {v4} = require("uuid");
+const {usersOnlineCreator, dtoMessageCreator} = require("../utils/utils.js");
+
 
 class ChatController {
-    // sockets;
-    //
-    // constructor() {
-    //     this.sockets = [];
-    // }
+    sockets;
+
+    constructor() {
+        this.sockets = [];
+    }
 
     connection = async (ws, req) => {
         try {
 
-            ws.on('message', chatService.onMessage(ws)
-                // async (message) => {
-                // let newMessageArr;
-                // const {type, data} = JSON.parse(message);
-                //
-                // switch (type) {
-                //     case 'auth': {
-                //         const tokenPayload = tokenService.verifyAccessToken(data.accessToken);
-                //         if (!tokenPayload) {
-                //             ws.close();
-                //         }
-                //         const userFromDB = await DAL.getUserById(tokenPayload.id);
-                //         if (!userFromDB) {
-                //             ws.close();
-                //         }
-                //
-                //         const {id, username} = userFromDB;
-                //         ws.id = v4();
-                //         ws.userId = id;
-                //         ws.username = username;
-                //
-                //         // send last 50 messages from DB
-                //         const lastMessagesArr = await DAL.getLastMessages(50);
-                //         const usersOnlineArr = usersOnlineCreator(this.sockets);
-                //         const chatObject = dtoMessageCreator(lastMessagesArr, usersOnlineArr);
-                //         ws.send(chatObject);
-                //
-                //         if (!this.sockets.find(socket => socket.userId === ws.userId)) {
-                //             // create admin message JOINED
-                //             newMessageArr = await chatService.adminMessageCreator(`${username} joined!`)
-                //         }
-                //
-                //         this.sockets.push(ws);
-                //
-                //         break;
-                //     }
-                //     case 'chat': {
-                //         if (!ws.id) {
-                //             ws.close();
-                //         }
-                //
-                //         newMessageArr = await chatService.userMessageCreator(ws.username, ws.userId, data.message);
-                //         break;
-                //     }
-                //     case 'ping':
-                //         break;
-                // }
-                //
-                // // SEND MESSAGE TO USERS
-                // if (newMessageArr && this.sockets.length) {
-                //     const usersOnline = usersOnlineCreator(this.sockets);
-                //     const dtoMessage = dtoMessageCreator(newMessageArr, usersOnline);
-                //
-                //     this.sockets.forEach(ws => {
-                //         ws.send(dtoMessage);
-                //     });
-                // }
-                // }
-            );
+            ws.on('message', async (message) => {
+                let newMessageArr;
+                const {type, data} = JSON.parse(message);
 
-            ws.on('close',chatService.onClose(ws)
-                // async () => {
-                    // // REMOVE SOCKET
-                    // this.sockets = this.sockets.filter(socket => socket.id !== ws.id);
-                    //
-                    // // CREATE MESSAGE IF NO USERS SOCKET IN SOCKETS ARRAY
-                    // if (!this.sockets.find(socket => socket.userId === ws.userId)) {
-                    //     const adminMessageArr = await chatService.adminMessageCreator(`${ws.username} left the chat!`);
-                    //
-                    //     // SEND MESSAGE TO USERS
-                    //     if (this.sockets.length) {
-                    //         const usersOnlineArr = usersOnlineCreator(this.sockets);
-                    //         const chatObject = dtoMessageCreator(adminMessageArr, usersOnlineArr);
-                    //
-                    //         this.sockets.forEach(ws => {
-                    //             ws.send(chatObject);
-                    //         });
-                    //     }
-                    // }
-                // }
-            );
+                switch (type) {
+                    case 'auth': {
+                        const tokenPayload = tokenService.verifyAccessToken(data.accessToken);
+                        if (!tokenPayload) {
+                            ws.close();
+                        }
+                        const userFromDB = await DAL.getUserById(tokenPayload.id);
+                        if (!userFromDB) {
+                            ws.close();
+                        }
+
+                        const {id, username} = userFromDB;
+                        ws.id = v4();
+                        ws.userId = id;
+                        ws.username = username;
+
+                        // send last 50 messages from DB
+                        const lastMessagesArr = await DAL.getLastMessages(50);
+                        const usersOnlineArr = usersOnlineCreator(this.sockets);
+                        const chatObject = dtoMessageCreator(lastMessagesArr, usersOnlineArr);
+                        ws.send(chatObject);
+
+                        if (!this.sockets.find(socket => socket.userId === ws.userId)) {
+                            // create admin message JOINED
+                            newMessageArr = await chatService.adminMessageCreator(`${username} joined!`)
+                        }
+
+                        this.sockets.push(ws);
+
+                        break;
+                    }
+                    case 'chat': {
+                        if (!ws.id) {
+                            ws.close();
+                        }
+
+                        newMessageArr = await chatService.userMessageCreator(ws.username, ws.userId, data.message);
+                        break;
+                    }
+                    case 'ping':
+                        break;
+                }
+
+                // SEND MESSAGE TO USERS
+                if (newMessageArr && this.sockets.length) {
+                    const usersOnline = usersOnlineCreator(this.sockets);
+                    const dtoMessage = dtoMessageCreator(newMessageArr, usersOnline);
+
+                    this.sockets.forEach(ws => {
+                        ws.send(dtoMessage);
+                    });
+                }
+                });
+
+            ws.on('close', async () => {
+                    // REMOVE SOCKET
+                    this.sockets = this.sockets.filter(socket => socket.id !== ws.id);
+
+                    // CREATE MESSAGE IF NO USERS SOCKET IN SOCKETS ARRAY
+                    if (!this.sockets.find(socket => socket.userId === ws.userId)) {
+                        const adminMessageArr = await chatService.adminMessageCreator(`${ws.username} left the chat!`);
+
+                        // SEND MESSAGE TO USERS
+                        if (this.sockets.length) {
+                            const usersOnlineArr = usersOnlineCreator(this.sockets);
+                            const chatObject = dtoMessageCreator(adminMessageArr, usersOnlineArr);
+
+                            this.sockets.forEach(ws => {
+                                ws.send(chatObject);
+                            });
+                        }
+                    }
+                });
 
         } catch (e) {
             console.log(e.message);
--- services/chat.service.js
+++ services/chat.service.js
@@ -1,19 +1,17 @@
 const dotenv = require('dotenv');
 const DAL = require("../db/dal.js");
-const tokenService = require("./token.service.js");
-const {v4} = require("uuid");
-const {usersOnlineCreator, dtoMessageCreator} = require("../utils/utils.js");
+// const tokenService = require("./token.service.js");
+// const {v4} = require("uuid");
+// const {usersOnlineCreator, dtoMessageCreator} = require("../utils/utils.js");
 
 class ChatService {
     adminName;
     adminId;
-    sockets;
 
     constructor() {
         dotenv.config();
         this.adminName = process.env.ADMIN_NAME || 'admin';
         this.adminId = +process.env.ADMIN_ID || 10;
-        this.sockets = [];
     }
 
     async adminMessageCreator(message) {
@@ -26,87 +24,87 @@ class ChatService {
         return userMessageArr;
     }
 
-    async onMessage(ws) {
-        return async (message) => {
-            let newMessageArr;
-            const {type, data} = JSON.parse(message);
+    // async onMessage(ws) {
+    //     return async (message) => {
+    //         let newMessageArr;
+    //         const {type, data} = JSON.parse(message);
+    //
+    //         switch (type) {
+    //             case 'auth': {
+    //                 const tokenPayload = tokenService.verifyAccessToken(data.accessToken);
+    //                 if (!tokenPayload) {
+    //                     ws.close();
+    //                 }
+    //                 const userFromDB = await DAL.getUserById(tokenPayload.id);
+    //                 if (!userFromDB) {
+    //                     ws.close();
+    //                 }
+    //
+    //                 const {id, username} = userFromDB;
+    //                 ws.id = v4();
+    //                 ws.userId = id;
+    //                 ws.username = username;
+    //
+    //                 // send last 50 messages from DB
+    //                 const lastMessagesArr = await DAL.getLastMessages(50);
+    //                 const usersOnlineArr = usersOnlineCreator(this.sockets);
+    //                 const chatObject = dtoMessageCreator(lastMessagesArr, usersOnlineArr);
+    //                 ws.send(chatObject);
+    //
+    //                 if (!this.sockets.find(socket => socket.userId === ws.userId)) {
+    //                     // create admin message JOINED
+    //                     newMessageArr = await this.adminMessageCreator(`${username} joined!`)
+    //                 }
+    //
+    //                 this.sockets.push(ws);
+    //
+    //                 break;
+    //             }
+    //             case 'chat': {
+    //                 if (!ws.id) {
+    //                     ws.close();
+    //                 }
+    //
+    //                 newMessageArr = await this.userMessageCreator(ws.username, ws.userId, data.message);
+    //                 break;
+    //             }
+    //             case 'ping':
+    //                 break;
+    //         }
+    //
+    //         // SEND MESSAGE TO USERS
+    //         if (newMessageArr && this.sockets.length) {
+    //             const usersOnline = usersOnlineCreator(this.sockets);
+    //             const dtoMessage = dtoMessageCreator(newMessageArr, usersOnline);
+    //
+    //             this.sockets.forEach(ws => {
+    //                 ws.send(dtoMessage);
+    //             });
+    //         }
+    //     }
+    // }
 
-            switch (type) {
-                case 'auth': {
-                    const tokenPayload = tokenService.verifyAccessToken(data.accessToken);
-                    if (!tokenPayload) {
-                        ws.close();
-                    }
-                    const userFromDB = await DAL.getUserById(tokenPayload.id);
-                    if (!userFromDB) {
-                        ws.close();
-                    }
-
-                    const {id, username} = userFromDB;
-                    ws.id = v4();
-                    ws.userId = id;
-                    ws.username = username;
-
-                    // send last 50 messages from DB
-                    const lastMessagesArr = await DAL.getLastMessages(50);
-                    const usersOnlineArr = usersOnlineCreator(this.sockets);
-                    const chatObject = dtoMessageCreator(lastMessagesArr, usersOnlineArr);
-                    ws.send(chatObject);
-
-                    if (!this.sockets.find(socket => socket.userId === ws.userId)) {
-                        // create admin message JOINED
-                        newMessageArr = await this.adminMessageCreator(`${username} joined!`)
-                    }
-
-                    this.sockets.push(ws);
-
-                    break;
-                }
-                case 'chat': {
-                    if (!ws.id) {
-                        ws.close();
-                    }
-
-                    newMessageArr = await this.userMessageCreator(ws.username, ws.userId, data.message);
-                    break;
-                }
-                case 'ping':
-                    break;
-            }
-
-            // SEND MESSAGE TO USERS
-            if (newMessageArr && this.sockets.length) {
-                const usersOnline = usersOnlineCreator(this.sockets);
-                const dtoMessage = dtoMessageCreator(newMessageArr, usersOnline);
-
-                this.sockets.forEach(ws => {
-                    ws.send(dtoMessage);
-                });
-            }
-        }
-    }
-
-    async onClose(ws) {
-        return async () => {
-            // REMOVE SOCKET
-            this.sockets = this.sockets.filter(socket => socket.id !== ws.id);
-
-            // CREATE MESSAGE IF NO USERS SOCKET IN SOCKETS ARRAY
-            if (!this.sockets.find(socket => socket.userId === ws.userId)) {
-                const adminMessageArr = await this.adminMessageCreator(`${ws.username} left the chat!`);
-
-                // SEND MESSAGE TO USERS
-                if (this.sockets.length) {
-                    const usersOnlineArr = usersOnlineCreator(this.sockets);
-                    const chatObject = dtoMessageCreator(adminMessageArr, usersOnlineArr);
-
-                    this.sockets.forEach(ws => {
-                        ws.send(chatObject);
-                    });
-                }
-            }
-        }
-    }
+    // async onClose(ws) {
+    //     return async () => {
+    //         // REMOVE SOCKET
+    //         this.sockets = this.sockets.filter(socket => socket.id !== ws.id);
+    //
+    //         // CREATE MESSAGE IF NO USERS SOCKET IN SOCKETS ARRAY
+    //         if (!this.sockets.find(socket => socket.userId === ws.userId)) {
+    //             const adminMessageArr = await this.adminMessageCreator(`${ws.username} left the chat!`);
+    //
+    //             // SEND MESSAGE TO USERS
+    //             if (this.sockets.length) {
+    //                 const usersOnlineArr = usersOnlineCreator(this.sockets);
+    //                 const chatObject = dtoMessageCreator(adminMessageArr, usersOnlineArr);
+    //
+    //                 this.sockets.forEach(ws => {
+    //                     ws.send(chatObject);
+    //                 });
+    //             }
+    //         }
+    //     }
+    // }
 }
 
 module.exports = new ChatService();
