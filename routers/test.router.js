457372b156509e5fb14f01feafcdc5108cbde457
”Aliaksandr-Batskalevich” <aliaksandr.batskalevich@gmail.com>
2023-06-09 12:13:13 +0300

    Added POST method in test endpoint

--- routers/test.router.js
+++ routers/test.router.js
@@ -5,6 +5,6 @@ const router = Router();
 
 router.get('/', testController.getTest);
 router.post('/', testController.createTest);
-router.post('/error', testController.errorTest);
+router.post('/error/', testController.errorTest);
 
 module.exports = router;
